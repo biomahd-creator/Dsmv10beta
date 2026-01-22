d5819678c4c3681381e9588baacbc574fa176d3e
biomahd-creator <biomahd@gmail.com>
2026-01-22 18:18:16 -0500

    Add tsup.config.ts to root directory
    Configure tsup for building the npm package with specified options.

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/npm-package/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  outDir: 'dist',
+  external: ['react', 'react-dom'],
+  treeshake: true,
+  minify: false, // Set to true for production
+  bundle: true,
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
+});
